595b34951622a00ab10fce528b23bb2939fed317
Vignesh E <evicky2002@gmail.com>
2022-06-21 11:16:50 +0530

    App bar removed from result activity

--- app/src/main/java/com/example/crediblecoursefinder/ResultActivity.java
+++ app/src/main/java/com/example/crediblecoursefinder/ResultActivity.java
@@ -7,13 +7,14 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        getSupportActionBar().hide();
         Intent intent = getIntent();
         String searchKey = intent.getStringExtra("SEARCHKEY");
         Toast.makeText(this, searchKey, Toast.LENGTH_SHORT).show();
+
     }
 }
